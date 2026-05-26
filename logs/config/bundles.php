3b0701e06cbe3a01e4efbca23c228c8e70be3f97
Eric Corbisier <contact@corbisier.fr>
2026-05-26 19:36:32 +0200

    Ajout du swagger

--- logs/config/bundles.php
+++ logs/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+
+Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
